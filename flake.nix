92a1be46bde7f30fb4c6f2c1d4b50841f1cbf03c
Veit Poigner <v.poigner@gmail.com>
2025-10-19 14:48:50 +0200

    chore: init

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
+            with pkgs; [
+              libxkbcommon
+              vulkan-loader
+              glfw-wayland
+              glfw
+              glew
+              mesa
+              libGL
+              libGLU
+              alsa-lib
+            ]
+          );
+          buildInputs = with pkgs; [
+            jdk
+            glfw-wayland
+            glfw
+            glew
+            mesa
+            libGL
+            libGLU
+            alsa-lib
+          ];
+        };
+      }
+    );
+}
